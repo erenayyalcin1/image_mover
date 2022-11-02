f7f72b597459895710768f72c417fb798525bd4a
erenay yalcin <95998994+erenayyalcin1@users.noreply.github.com>
2022-11-02 13:01:01 +0300

    Add files via upload

---
+++ odev1/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace odev1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTN_YUKARI = new System.Windows.Forms.Button();
+            this.BTN_sag = new System.Windows.Forms.Button();
+            this.BTN_alt = new System.Windows.Forms.Button();
+            this.BTN_sol = new System.Windows.Forms.Button();
+            this.PB_Fotograf_Alani = new System.Windows.Forms.PictureBox();
+            this.BTN_mode_degistirme = new System.Windows.Forms.Button();
+            this.BTN_foto_yukle = new System.Windows.Forms.Button();
+            this.BTN_Yuksekligi_Artirir = new System.Windows.Forms.Button();
+            this.BTN_Genisligi_Artirir = new System.Windows.Forms.Button();
+            this.BTN_Yuksekligi_Azaltir = new System.Windows.Forms.Button();
+            this.BTN_Genisligi_Azaltir = new System.Windows.Forms.Button();
+            this.Label_Size = new System.Windows.Forms.Label();
+            this.Label_Location = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_Fotograf_Alani)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BTN_YUKARI
+            // 
+            this.BTN_YUKARI.Cursor = System.Windows.Forms.Cursors.PanNorth;
+            this.BTN_YUKARI.Location = new System.Drawing.Point(108, 278);
+            this.BTN_YUKARI.Name = "BTN_YUKARI";
+            this.BTN_YUKARI.Size = new System.Drawing.Size(75, 23);
+            this.BTN_YUKARI.TabIndex = 0;
+            this.BTN_YUKARI.Text = "^";
+            this.BTN_YUKARI.UseVisualStyleBackColor = true;
+            this.BTN_YUKARI.Click += new System.EventHandler(this.BTN_YUKARI_Click);
+            // 
+            // BTN_sag
+            // 
+            this.BTN_sag.Cursor = System.Windows.Forms.Cursors.PanEast;
+            this.BTN_sag.Location = new System.Drawing.Point(181, 315);
+            this.BTN_sag.Name = "BTN_sag";
+            this.BTN_sag.Size = new System.Drawing.Size(75, 23);
+            this.BTN_sag.TabIndex = 1;
+            this.BTN_sag.Text = "->";
+            this.BTN_sag.UseVisualStyleBackColor = true;
+            this.BTN_sag.Click += new System.EventHandler(this.BTN_sag_Click);
+            // 
+            // BTN_alt
+            // 
+            this.BTN_alt.Cursor = System.Windows.Forms.Cursors.PanSouth;
+            this.BTN_alt.Location = new System.Drawing.Point(108, 358);
+            this.BTN_alt.Name = "BTN_alt";
+            this.BTN_alt.Size = new System.Drawing.Size(75, 23);
+            this.BTN_alt.TabIndex = 2;
+            this.BTN_alt.Text = "V";
+            this.BTN_alt.UseVisualStyleBackColor = true;
+            this.BTN_alt.Click += new System.EventHandler(this.BTN_alt_Click);
+            // 
+            // BTN_sol
+            // 
+            this.BTN_sol.Cursor = System.Windows.Forms.Cursors.PanWest;
+            this.BTN_sol.Location = new System.Drawing.Point(33, 315);
+            this.BTN_sol.Name = "BTN_sol";
+            this.BTN_sol.Size = new System.Drawing.Size(75, 23);
+            this.BTN_sol.TabIndex = 3;
+            this.BTN_sol.Text = "<-";
+            this.BTN_sol.UseVisualStyleBackColor = true;
+            this.BTN_sol.Click += new System.EventHandler(this.BTN_sol_Click);
+            // 
+            // PB_Fotograf_Alani
+            // 
+            this.PB_Fotograf_Alani.Location = new System.Drawing.Point(300, 12);
+            this.PB_Fotograf_Alani.MaximumSize = new System.Drawing.Size(238, 202);
+            this.PB_Fotograf_Alani.Name = "PB_Fotograf_Alani";
+            this.PB_Fotograf_Alani.Size = new System.Drawing.Size(238, 202);
+            this.PB_Fotograf_Alani.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.PB_Fotograf_Alani.TabIndex = 4;
+            this.PB_Fotograf_Alani.TabStop = false;
+            this.PB_Fotograf_Alani.LocationChanged += new System.EventHandler(this.PB_Fotograf_Alani_LocationChanged);
+            this.PB_Fotograf_Alani.SizeChanged += new System.EventHandler(this.PB_Fotograf_Alani_SizeChanged);
+            // 
+            // BTN_mode_degistirme
+            // 
+            this.BTN_mode_degistirme.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BTN_mode_degistirme.Location = new System.Drawing.Point(383, 290);
+            this.BTN_mode_degistirme.Name = "BTN_mode_degistirme";
+            this.BTN_mode_degistirme.Size = new System.Drawing.Size(118, 48);
+            this.BTN_mode_degistirme.TabIndex = 5;
+            this.BTN_mode_degistirme.Text = "Mode Degistir";
+            this.BTN_mode_degistirme.UseVisualStyleBackColor = true;
+            this.BTN_mode_degistirme.Click += new System.EventHandler(this.BTN_mode_degistirme_Click);
+            // 
+            // BTN_foto_yukle
+            // 
+            this.BTN_foto_yukle.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BTN_foto_yukle.Location = new System.Drawing.Point(383, 358);
+            this.BTN_foto_yukle.Name = "BTN_foto_yukle";
+            this.BTN_foto_yukle.Size = new System.Drawing.Size(118, 54);
+            this.BTN_foto_yukle.TabIndex = 6;
+            this.BTN_foto_yukle.Text = "Foto Yukle";
+            this.BTN_foto_yukle.UseVisualStyleBackColor = true;
+            this.BTN_foto_yukle.Click += new System.EventHandler(this.BTN_foto_yukle_Click);
+            // 
+            // BTN_Yuksekligi_Artirir
+            // 
+            this.BTN_Yuksekligi_Artirir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BTN_Yuksekligi_Artirir.Location = new System.Drawing.Point(548, 290);
+            this.BTN_Yuksekligi_Artirir.Name = "BTN_Yuksekligi_Artirir";
+            this.BTN_Yuksekligi_Artirir.Size = new System.Drawing.Size(95, 48);
+            this.BTN_Yuksekligi_Artirir.TabIndex = 7;
+            this.BTN_Yuksekligi_Artirir.Text = "Yuksekligi Artirir";
+            this.BTN_Yuksekligi_Artirir.UseVisualStyleBackColor = true;
+            this.BTN_Yuksekligi_Artirir.Click += new System.EventHandler(this.BTN_Yuksekligi_Artirir_Click);
+            // 
+            // BTN_Genisligi_Artirir
+            // 
+            this.BTN_Genisligi_Artirir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BTN_Genisligi_Artirir.Location = new System.Drawing.Point(669, 290);
+            this.BTN_Genisligi_Artirir.Name = "BTN_Genisligi_Artirir";
+            this.BTN_Genisligi_Artirir.Size = new System.Drawing.Size(90, 48);
+            this.BTN_Genisligi_Artirir.TabIndex = 8;
+            this.BTN_Genisligi_Artirir.Text = "Genisligi Artirir";
+            this.BTN_Genisligi_Artirir.UseVisualStyleBackColor = true;
+            this.BTN_Genisligi_Artirir.Click += new System.EventHandler(this.BTN_Genisligi_Artirir_Click);
+            // 
+            // BTN_Yuksekligi_Azaltir
+            // 
+            this.BTN_Yuksekligi_Azaltir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BTN_Yuksekligi_Azaltir.Location = new System.Drawing.Point(548, 357);
+            this.BTN_Yuksekligi_Azaltir.Name = "BTN_Yuksekligi_Azaltir";
+            this.BTN_Yuksekligi_Azaltir.Size = new System.Drawing.Size(95, 55);
+            this.BTN_Yuksekligi_Azaltir.TabIndex = 9;
+            this.BTN_Yuksekligi_Azaltir.Text = "Yuksekligi azaltir";
+            this.BTN_Yuksekligi_Azaltir.UseVisualStyleBackColor = true;
+            this.BTN_Yuksekligi_Azaltir.Click += new System.EventHandler(this.BTN_Yuksekligi_Azaltir_Click);
+            // 
+            // BTN_Genisligi_Azaltir
+            // 
+            this.BTN_Genisligi_Azaltir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BTN_Genisligi_Azaltir.Location = new System.Drawing.Point(669, 357);
+            this.BTN_Genisligi_Azaltir.Name = "BTN_Genisligi_Azaltir";
+            this.BTN_Genisligi_Azaltir.Size = new System.Drawing.Size(90, 55);
+            this.BTN_Genisligi_Azaltir.TabIndex = 10;
+            this.BTN_Genisligi_Azaltir.Text = "Genisligi azaltir";
+            this.BTN_Genisligi_Azaltir.UseVisualStyleBackColor = true;
+            this.BTN_Genisligi_Azaltir.Click += new System.EventHandler(this.BTN_Genisligi_Azaltir_Click);
+            // 
+            // Label_Size
+            // 
+            this.Label_Size.AutoSize = true;
+            this.Label_Size.Location = new System.Drawing.Point(12, 9);
+            this.Label_Size.Name = "Label_Size";
+            this.Label_Size.Size = new System.Drawing.Size(0, 17);
+            this.Label_Size.TabIndex = 11;
+            this.Label_Size.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Label_Location
+            // 
+            this.Label_Location.AutoSize = true;
+            this.Label_Location.Location = new System.Drawing.Point(13, 30);
+            this.Label_Location.Name = "Label_Location";
+            this.Label_Location.Size = new System.Drawing.Size(0, 17);
+            this.Label_Location.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 450);
+            this.Controls.Add(this.Label_Location);
+            this.Controls.Add(this.Label_Size);
+            this.Controls.Add(this.BTN_Genisligi_Azaltir);
+            this.Controls.Add(this.BTN_Yuksekligi_Azaltir);
+            this.Controls.Add(this.BTN_Genisligi_Artirir);
+            this.Controls.Add(this.BTN_Yuksekligi_Artirir);
+            this.Controls.Add(this.BTN_foto_yukle);
+            this.Controls.Add(this.BTN_mode_degistirme);
+            this.Controls.Add(this.PB_Fotograf_Alani);
+            this.Controls.Add(this.BTN_sol);
+            this.Controls.Add(this.BTN_alt);
+            this.Controls.Add(this.BTN_sag);
+            this.Controls.Add(this.BTN_YUKARI);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PB_Fotograf_Alani)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTN_YUKARI;
+        private System.Windows.Forms.Button BTN_sag;
+        private System.Windows.Forms.Button BTN_sol;
+        private System.Windows.Forms.PictureBox PB_Fotograf_Alani;
+        private System.Windows.Forms.Button BTN_mode_degistirme;
+        private System.Windows.Forms.Button BTN_foto_yukle;
+        private System.Windows.Forms.Button BTN_Yuksekligi_Artirir;
+        private System.Windows.Forms.Button BTN_Genisligi_Artirir;
+        private System.Windows.Forms.Button BTN_Yuksekligi_Azaltir;
+        private System.Windows.Forms.Button BTN_Genisligi_Azaltir;
+        private System.Windows.Forms.Label Label_Size;
+        private System.Windows.Forms.Button BTN_alt;
+        private System.Windows.Forms.Label Label_Location;
+    }
+}
+
---
+++ odev1/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace odev1
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// Mode degistirme butonuna basildikca size modunu degistiriyorum sirasiyla  autosize centerimage zoom normal strechimage autosize
+        /// </summary>
+
+        private void BTN_mode_degistirme_Click(object sender, EventArgs e)
+
+
+        {
+            
+            byte a = (byte)PB_Fotograf_Alani.SizeMode; //Anlik olarak pictureboxun size modu
+            if (a==4)
+            {
+                a = 0;   // 5 . opsiyon olmadigi icin 0 a geri donecek 
+            }
+            else
+            {
+                a += 1;  // a yi birer artirir.
+            }
+            //a artik yeni size modun sayisini temsil eder.
+
+            PB_Fotograf_Alani.SizeMode = (PictureBoxSizeMode)a;
+           
+        }
+
+        private void BTN_foto_yukle_Click(object sender, EventArgs e)
+        {
+            PB_Fotograf_Alani.Image = FotoIslemleri.FotoSectirtVeGetir();// kullaniciya bir fotograf sectirir ve fotografi alana atar.
+
+
+        }
+        /// <summary>
+        /// Yuksekligi 10 artiracak.
+        /// </summary>
+        private void BTN_Yuksekligi_Artirir_Click(object sender, EventArgs e)
+        {
+
+            // picture boksimin sizenin yuksekligini yukseklik ya atayacagim 
+            int yukseklik = PB_Fotograf_Alani.Size.Height;
+            // picture baksin sizenin genisligi b ye atanacak
+            int genislik = PB_Fotograf_Alani.Size.Width;
+            Size YeniSize= new Size(genislik, yukseklik + 10);// PB_Fotograf_Alani'in yeni size'ini ifade eder.
+            PB_Fotograf_Alani.MaximumSize = YeniSize;
+            PB_Fotograf_Alani.Size = YeniSize;
+        }
+        /// <summary>
+        /// Yuksekligi 10 azaltacak.
+        /// </summary>
+        private void BTN_Yuksekligi_Azaltir_Click(object sender, EventArgs e)
+        {
+            //fotograf alaninin genisligini genislige atadik.
+            int genislik = PB_Fotograf_Alani.Size.Width;
+            //fotograf alaninin yuksekligini yukseklige atadik.
+            int yukseklik = PB_Fotograf_Alani.Size.Height; 
+            Size YeniSize= new Size(genislik, yukseklik - 10); //size in icinden yeni size olusturduk.
+            PB_Fotograf_Alani.MaximumSize = YeniSize;
+            PB_Fotograf_Alani.Size = YeniSize;
+        }
+        /// <summary>
+        /// Genisligi 10 artiracak.
+        /// </summary>
+        private void BTN_Genisligi_Artirir_Click(object sender, EventArgs e)
+        {
+            //fotograf alanin genisligini genislige atadik
+            int genislik = PB_Fotograf_Alani.Size.Width;
+            // fotograf alanindan yuksekligi yukseklige atadik 
+            int yukseklik = PB_Fotograf_Alani.Size.Height;
+            Size YeniSize = new Size(genislik + 10, yukseklik);
+            PB_Fotograf_Alani.MaximumSize = YeniSize;
+            PB_Fotograf_Alani.Size = YeniSize;
+        }
+        /// <summary>
+        /// Genisligi 10 azaltacak
+        /// </summary>
+        private void BTN_Genisligi_Azaltir_Click(object sender, EventArgs e)
+        {
+            //Fotograf alanin genisligini genisligine atadik.
+            int genislik = PB_Fotograf_Alani.Size.Width;
+            //Fotograf alanin yuksekligini yukseklige atadik.
+            int yukseklik = PB_Fotograf_Alani.Size.Height;
+            Size YeniSize = new Size(genislik-10, yukseklik);
+            PB_Fotograf_Alani.MaximumSize = YeniSize;
+            PB_Fotograf_Alani.Size = YeniSize;
+        }
+
+        private void BTN_sag_Click(object sender, EventArgs e)
+        {
+            //fotograf alaninda ki lokasyonun x degerini x e atadim.
+            int x = PB_Fotograf_Alani.Location.X;
+            //fotograf alaninda ki lokasyonun y degerini y e atadim.
+            int y = PB_Fotograf_Alani.Location.Y;
+
+            //sag butona tikladigimda picturebox 10 piksel saga kayar.
+            PB_Fotograf_Alani.Location = new Point(x+10, y);
+        }
+
+        private void BTN_sol_Click(object sender, EventArgs e)
+        {
+            //fotograf alaninda ki lokasyonun x degerini x e atar.
+            int x = PB_Fotograf_Alani.Location.X;
+            //fotograf alaninda ki lokasyonun y degerini y e atar.
+            int y = PB_Fotograf_Alani.Location.Y;
+
+            // sol butona tikladiginda pictureBox 10 piksel sola kayar 
+            PB_Fotograf_Alani.Location = new Point(x - 10, y);
+        }
+
+        private void BTN_alt_Click(object sender, EventArgs e)
+        {
+            //fotograf alaninda ki lokasyonun x degerini x e atar.
+            int x = PB_Fotograf_Alani.Location.X;
+            //fotograf alaninda ki lokasyonun y degerini y e atar.
+            int y = PB_Fotograf_Alani.Location.Y;
+
+            // alt butona  tiklandiginda picture box 10 piksel alt kisma kayar.
+            PB_Fotograf_Alani.Location = new Point(x, y + 10);
+        }
+
+        private void BTN_YUKARI_Click(object sender, EventArgs e)
+        {
+            //fotograf alaninda ki lokasyonun x degerini x e atar.
+            int x = PB_Fotograf_Alani.Location.X;
+            //fotograf alaninda ki lokasyonun y degerini y e atar.
+            int y = PB_Fotograf_Alani.Location.Y;
+
+            // ust butona  tiklandiginda picture box 10 piksel ust kisma kayar.
+            PB_Fotograf_Alani.Location = new Point(x, y - 10);
+        }
+
+        private void PB_Fotograf_Alani_SizeChanged(object sender, EventArgs e)
+        {
+            //Label_Text.Text = PB_Fotograf_Alani.Size.Width + "," + PB_Fotograf_Alani.Size.Height;
+            Label_Size.Text = $"{PB_Fotograf_Alani.Size.Width},{PB_Fotograf_Alani.Size.Height}";
+        }
+
+        private void PB_Fotograf_Alani_LocationChanged(object sender, EventArgs e)
+        {
+            Label_Location.Text = $"x:{PB_Fotograf_Alani.Location.X},y:{PB_Fotograf_Alani.Location.Y}";// x:10 , y:20
+        }
+    }
+    // Örnek kullanım: FotoIslemleri.FotoSectirtVeGetir()
+    public class FotoIslemleri
+    {
+        /// <summary>
+        /// Kullanıcıya bir fotoğraf seçtirir ve seçtiği fotoğrafı döndürür. Eğer kullanıcı bir fotoğraf seçmezse NULL DÖNDÜRÜR.
+        /// </summary>
+        /// <returns>Kullanıcı bir fotoğraf seçerse, seçtiği fotoğrafı döndürür. Eğer seçmezse NULL Döndürür.</returns>
+        public static Image FotoSectirtVeGetir()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.CheckPathExists = true;
+            openFileDialog.Multiselect = false;
+            openFileDialog.Filter = "jpg dosyalari (.jpg)|*.jpg|png dosyalari (.png)|*.png|jpeg dosyalari (.jpeg)|*.jpeg";
+            openFileDialog.Title = "BIR FOTOGRAF SECINIZ.";
+
+            return openFileDialog.ShowDialog() == DialogResult.OK ? Image.FromFile(openFileDialog.FileName) : null;
+        }
+    }
+}
